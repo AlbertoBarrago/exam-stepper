b264f7c56ffe3cf3e436ef1f1a9d9c023dd89ae3
Alberto Barrago <albertobarrago@gmail.com>
2025-07-24 12:11:32 +0200

    ♻️ refactor: Introduce `WritingTask` component and update `WritingStep` to use new structure
    - Created reusable `WritingTask` component to handle text input with word count validation and progress tracking.
    - Extracted and replaced `Props` type with `WritingTypes` for improved type declaration and reuse.
    - Simplified `WritingStep` by integrating the new `WritingTask` component and its related functionalities.

--- app/exam/[attemptId]/steps/Writing/WritingStep.tsx
+++ app/exam/[attemptId]/steps/Writing/WritingStep.tsx
@@ -1,20 +1,31 @@
 'use client';
 
-type Props = { title: string, onNextAction: () => void };
+import WritingTask from "@/components/step/WritingTask";
+import {WritingTypes} from "@/types/writingTypes";
 
-export default function WritingStep({ title, onNextAction }: Props) {
 
-    const handleNext = () => {
+export default function WritingStep({ onNextAction }: WritingTypes) {
+
+    const handleTextChange = (text: string, wordCount: number) => {
+        console.log('Text changed:', { text, wordCount });
+    };
+
+    const submitToAi = () => {
+
+        // TODO: add AI analysis for text, than go Next...
+
         onNextAction();
     }
 
     return (
-        <>
-
-            {title}
-            <button className="btn mt-6 ml-4" onClick={handleNext}>
-                Next →
-            </button>
-        </>
+        <WritingTask
+            title="Write a clear and compelling job advertisement."
+            placeholder="Start your answer here..."
+            minWords={50}
+            maxWords={150}
+            onTextChange={handleTextChange}
+            onSubmit={submitToAi}
+            buttonText="Submit"
+        />
     )
 }
---
+++ app/types/writingTypes.ts
@@ -0,0 +1,1 @@
+export type WritingTypes = { title: string, onNextAction: () => void };
