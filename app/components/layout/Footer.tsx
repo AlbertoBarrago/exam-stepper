7fd4723c3dfbfb14377d0068647b78513a16a445
Alberto Barrago <albertobarrago@gmail.com>
2025-08-04 10:40:09 +0200

    ✨ **feat(api, ui): Enhance error handling, user redirection, and add version to Footer**
    - Added try-catch block in login route to handle unexpected errors, returning appropriate 500 status response.
    - Updated `Main` component to redirect unauthenticated users to login using `useEffect` and return `null` for invalid states.
    - Appended app version dynamically to `Footer` using `package.json`.

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import packageJson from '../../../package.json';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  return <footer className="py-4 text-center text-sm text-gray-500">alBz ©{currentYear}</footer>;
+  return (
+    <footer className="py-4 text-center text-sm text-gray-500">
+      alBz ©{currentYear} v{packageJson.version}
+    </footer>
+  );
 };
 
 export default Footer;
